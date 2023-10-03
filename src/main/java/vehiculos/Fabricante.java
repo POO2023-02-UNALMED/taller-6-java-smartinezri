4cf6b9239668178996bd0dd3fb01d4075dcb207b
smartinezri <smartinezri@unal.edu.co>
2023-10-02 21:29:57 -0500

    aa

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,13 +1,19 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Fabricante {
 
 	private String nombre;
 	private Pais pais;
+	private int totalFabricados;
+	private static ArrayList<Fabricante> fabricas = new ArrayList<>();
 	
 	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre;
 		this.pais = pais;
+		fabricas.add(this);
+		totalFabricados++;
+		pais.totalFabricados++;
 	}
 	
 	public void setNombre(String nombre) {
@@ -24,6 +30,17 @@ public class Fabricante {
 	
 	public Pais getPais() {
 		return pais;
+	}
+	
+	public String fabricaMayorVentas() {
+		Fabricante fabrica = fabricas.get(0);
+		
+		for(int i = 1; i<fabricas.size(); i++) {
+			if(fabricas.get(i).totalFabricados > fabrica.totalFabricados) {
+				fabrica = fabricas.get(i);
+			}
+		}
+		return fabrica.nombre;
 	}
 	
 }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,11 +1,15 @@
 package vehiculos;
+import java.util.ArrayList;
 
 public class Pais {
 
 	private String nombre;
+	int totalFabricados;
+	private static ArrayList<Pais> paises = new ArrayList<>();
 	
 	public Pais(String nombre) {
 		this.nombre = nombre;
+		paises.add(this);
 	}
 	
 	public void setNombre(String nombre) {
@@ -14,6 +18,17 @@ public class Pais {
 	
 	public String getNombre() {
 		return nombre;
+	}
+	
+	public String paisMasVendedor() {
+		Pais pais = paises.get(0);
+		
+		for(int i = 1; i<paises.size(); i++) {
+			if(paises.get(i).totalFabricados > pais.totalFabricados) {
+				pais = paises.get(i);
+			}
+		}
+		return pais.nombre;
 	}
 	
 }
